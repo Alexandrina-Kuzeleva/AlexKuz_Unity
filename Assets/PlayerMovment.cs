656a6cf4270c573d09e39c369763e43b1db30f38
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2025-02-28 22:23:47 +0300

    Laba 3
    Add collectible subj
    Add counter
    Add new system of jumping

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -1,36 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5f;
     private float Move;
-    public float jump;
+    public float jump = 300f;
     private Rigidbody2D rb;
-    public bool isJumping;
+    private bool isJumping;
     public Animator animator;
+    public float raycastDistance = 0.2f;
+    public LayerMask groundLayer; 
+    public int collectibleCount = 0;
+    public Text collectibleText;
 
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        UpdateCollectibleUI();
     }
 
-    [System.Obsolete]
-    void Update() {
+    void Update()
+    {
         Move = Input.GetAxis("Horizontal");
-
-        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
-
+        rb.linearVelocity = new Vector2(speed * Move, rb.linearVelocity.y);
         animator.SetBool("IsRunning", Move != 0);
 
-        if (Input.GetButtonDown("Jump") && isJumping == false) 
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-            animator.SetBool("IsJumping", true);
+            if (IsGrounded())
+            {
+                rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
+                isJumping = true;
+                animator.SetBool("IsJumping", true);
+            }
         }
-        
+
+        if (IsGrounded() && isJumping)
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false);
+        }
+
         if (Move > 0)
         {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -39,23 +51,44 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
-
     }
-    
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Ground"))
+
+    bool IsGrounded()
+    {
+        Vector2 rayStart = new Vector2(transform.position.x, transform.position.y - 0.85f);
+        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector2.down, raycastDistance, groundLayer);
+        if (hit.collider != null)
         {
-            isJumping = false;
-            animator.SetBool("IsJumping", false);
+            float angle = Vector2.Angle(hit.normal, Vector2.up);
+            return angle < 45f; 
+        }
+        return false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            collectibleCount++; 
+            Destroy(other.gameObject); 
+            UpdateCollectibleUI();
         }
     }
 
-    private void OnCollisionExit2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Ground"))
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector2 rayStart = new Vector2(transform.position.x, transform.position.y - 0.85f);
+        Gizmos.DrawRay(rayStart, Vector2.down * raycastDistance);
+    }
+
+    void UpdateCollectibleUI()
         {
-            isJumping = true;
-            animator.SetBool("IsJumping", true);
-        }
+            if (collectibleText != null)
+            {
+                collectibleText.text = "Count: " + collectibleCount;
+            }
     }
 }
 
+    
